5f961c55aed9f918a9f3f61feeb5cf56c4fac7d7
Stephen McQuaid <smcquaid@godaddy.com>
2015-07-16 18:07:53 -0400

    Only one accordion should be open at a time.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
   $( ".header" ).next( ".content" ).hide();
-  
+
   $(".header").click(function () {
+    $( ".header" ).next( ".content" ).hide();
     $(this).next(".content").slideToggle(500);
   });
 });
