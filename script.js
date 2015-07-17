1148e95b5dd7ae94bb90b45a8c1c96a256747ccd
germy <germyjermy@yahoo.com>
2015-07-17 03:54:56 -0400

    smooth accordion collapsing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   $( ".header" ).next( ".content" ).hide();
 
   $(".header").click(function () {
-    $( ".header" ).next( ".content" ).hide();
+    $( ".header" ).next( ".content" ).hide(500);
     $(this).next(".content").slideToggle(500);
   });
 });
